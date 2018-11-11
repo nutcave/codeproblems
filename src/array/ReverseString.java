16c8403a82011c82c37aef6806e21d2979b6106e
Nutcave <proska8@gmail.com>
2018-11-11 19:31:16 +0000

    update

--- src/array/ReverseString.java
+++ src/array/ReverseString.java
@@ -11,65 +11,36 @@ public class ReverseString {
 
     public static class Solution {
 
-
-
         public String reverseWords(String a) {
-            StringBuilder sb= new StringBuilder();
+            char[] str=a.toCharArray();
 
-            char[] str= a.toCharArray();
-
-            int start=0, end=0;
-            boolean newWord=true;
-
-
-            for(int i=str.length-1; i >=0 ; --i){
-
-                if(i-1<0 || str[i]==' '){
-                    newWord=true;
-                    String s=a.substring(i+1,end);
-                    sb.append(s);
-                    sb.append(" ");
-
-
-                }
-
-
-
-
-                if(newWord){
-
-                    end=i+1;
-                    newWord=false;
-                }
-
-            }
-
-
-
-            return sb.toString();
-        }
-
-        public void reverseWords2(char[] s) {
             int i=0;
-            for(int j=0; j<s.length; j++){
-                if(s[j]==' '){
-                    reverse(s, i, j-1);
+            int j=0;
+            while(j<str.length){
+                if(str[j] == ' '){
+
+                    reverse(str,i,j-1);
                     i=j+1;
                 }
+                j++;
+
             }
 
-            reverse(s, i, s.length-1);
+            reverse(str,i,str.length-1);
+            reverse(str,0,str.length-1);
+            return String.valueOf(str);
 
-            reverse(s, 0, s.length-1);
+
         }
 
-        public void reverse(char[] s, int i, int j){
-            while(i<j){
-                char temp = s[i];
-                s[i]=s[j];
-                s[j]=temp;
-                i++;
-                j--;
+        void reverse(char[] str,int i, int j){
+            while(i < j){
+                char tmp=str[i];
+                str[i]=str[j];
+                str[j]=tmp;
+                --j;
+                ++i;
+
             }
         }
 
