4a86d5d20915e5bf04925fc934e0a1ae5e699e8b
Nutcave <proska8@gmail.com>
2018-11-12 00:03:24 +0000

    update

---
+++ src/tree/LowestCommonAncestor.java
@@ -0,0 +1,28 @@
+package tree;
+
+import util.TreeNode;
+
+/**
+ * Created by Yousifk on 11-Nov-18.
+ */
+public class LowestCommonAncestor {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null)
+            return null;
+
+        if(root==p || root==q)
+            return root;
+
+        TreeNode l = lowestCommonAncestor(root.left, p, q);
+        TreeNode r = lowestCommonAncestor(root.right, p, q);
+
+        if(l!=null&&r!=null){
+            return root;
+        }else if(l==null&&r==null){
+            return null;
+        }else{
+            return l==null?r:l;
+        }
+    }
+}
